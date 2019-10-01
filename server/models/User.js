786b95264b56229a833b3d1598fde44d80a6c84b
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-10-02 04:39:45 +0700

    remove mongoose unique validator dependency

--- server/models/User.js
+++ server/models/User.js
@@ -1,20 +1,33 @@
 'use strict'
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
-const uniqueValidator = require('mongoose-unique-validator')
 const { hash } = require('../helpers/bcryptjs')
 
 const UserSchema = new Schema({
     username: {
         type: String,
         required: [true, 'Username required'],
-        unique: process.env.NODE_ENV === 'test' ? false : true
+        validate: {
+            validator(value) {
+                return new Promise((resolve, reject) => {
+                    User.findOne({ username: value }).then(User => User ? resolve(false) : resolve(true))
+                });
+            },
+            message: "Username is already taken."
+        }
     },
     email: {
         type: String,
         required: [true, 'Email required.'],
         match: [/^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Invalid email format."],
-        unique: process.env.NODE_ENV === 'test' ? false : true
+        validate: {
+            validator(value) {
+                return new Promise((resolve, reject) => {
+                    User.findOne({ email: value }).then(User => User ? resolve(false) : resolve(true))
+                });
+            },
+            message: "Email is already taken."
+        }
     },
     password: {
         type: String,
@@ -23,7 +36,6 @@ const UserSchema = new Schema({
     }
 }, { timestamps: true })
 
-UserSchema.plugin(uniqueValidator, { message: "{PATH} must be unique" })
 
 UserSchema.pre('save', function (next) {
     this.password = hash(this.password)
